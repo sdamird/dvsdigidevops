df87027111ddbdf9b068c3eb3731bad56116b9c4
sdamird <damiaws2024@gmail.com>
2025-01-21 23:36:28 +0530

    add

---
+++ shell-script/05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo " Please enter username"
+read -s USERNAME
+echo "username entered: $USERNAME"
+echo "Please enter the password::"
+read -s PASSWORD
+echo "Entered password: $PASSWORD"
---
+++ shell-script/08-specialvariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " All variables passed $@"
+echo :Number of variables $#"
+echo "script name $0"
+echo " Present working directory: $PWD"
+echo "Home Directory of current user: $HOME"
+echo " which user is running the script :$USER"
+echo "Process id of current script: $$"
+sleep 60 &
+echo "process id of last command in background: $!"
