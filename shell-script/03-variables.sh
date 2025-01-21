f71c3f2d13ce1c36c1948be820bfabd3dd1e44b1
sdamird <damiaws2024@gmail.com>
2025-01-21 15:55:28 +0000

    added

---
+++ shell-script/03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSION1=ramesh
+PERSION2=Subbu
+echo "$PERSION1:: Hi $PERSION2"
+echo "$PERSION2:: Hello $PERSION1"
+echo "$PERSION1:: How are you doing?"
+echo "$PERSION2: I am good. How are you?"
---
+++ shell-script/04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSION1=$1
+PERSION2=$2
+echo "$PERSION1:: Hi $PERSION2"
+echo "$PERSION2:: Hello $PERSION1"
+echo "$PERSION1:: How are you doing?"
+echo "$PERSION2: I am good. How are you?"
