775b9b021f6f7cf5e18ffd79b02c16360ac85a7c
sdamird <damiaws2024@gmail.com>
2025-01-21 23:09:13 +0530

    arrays

---
+++ shell-script/07-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+MOVIES=("pushpa" "rrr" "devara")
+#index starts from 0, size is 3
+echo "First move: ${MOVIES[0]}"
+echo " Second movie: ${MOVIES[1]}"
+echo " Third movie: ${MOVIES[2]}"
