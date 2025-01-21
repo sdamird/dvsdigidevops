280a451c42276a487b30ecbb3d5ab79f3e1450d0
sdamird <damiaws2024@gmail.com>
2025-01-22 02:15:30 +0530

    1

--- shell-script/10-install-script.sh
+++ shell-script/10-install-script.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-USERID=(id -u)
+USERID=$(id -u)
 if [ "$USERID" -ne 0 ];then 
   echo "Error: you must have sudo privilge to install the package"
   exit 1 #other than 0
   fi
+  dnf list installed mysql
+ if [ $? -ne 0 ] ;then
+   dnf install mysql -y 
+   if [ $? -ne 0 ];then 
+    echo " Installing ...FAILURE"
+    exit 1
+    else 
+    echo " Insfalling MySql Success"
+    fi
+    else 
+      
+   echo " mysql already installed "
+   fi
+
+   #Installing the git
+   dnf list installed git 
+   if [ $? -ne 0 ]
+   then 
+    dnf install git -y 
+    if [ $? -ne 0 ]
+    then 
+      echo " Installing Git ..Failure"
+      exit 1
+    else 
+      echo " Installling git success "
+    fi
+    else 
+     echo " Git already installed "
+     fi
+
