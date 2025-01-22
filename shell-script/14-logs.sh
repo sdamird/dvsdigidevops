ce39c444c16a2b6bafafac72ca424382a2a28429
sdamird <damiaws2024@gmail.com>
2025-01-22 07:34:57 +0000

    logs.sh

--- shell-script/14-logs.sh
+++ shell-script/14-logs.sh
@@ -3,10 +3,12 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 LOG_FOLDER="/var/log/shellscripts-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+mkdir -p "$LOG_FOLDER"
 VALIDATE(){
      if [ $1 -ne 0 ];then 
     echo -e " $2 ...$R FAILURE"
@@ -16,25 +18,25 @@ VALIDATE(){
     fi
      
 }
-echo " Script started executing at $TIMESTAMP" &>>$OG_FILE_NAME
+echo " Script started executing at $TIMESTAMP" &>> "$LOG_FILE_NAME"
 if [ "$USERID" -ne 0 ];then 
   echo "Error: you must have sudo privilge to install the package"
   exit 1 #other than 0
   fi
-  dnf list installed mysql   &>>$OG_FILE_NAME
+  dnf list installed mysql   &>> "$LOG_FILE_NAME"
  if [ $? -ne 0 ] ;then
-   dnf install mysql -y 
+   dnf install mysql -y &>> "$LOG_FILE_NAME"
    VALIDATE $? "Installing MySql"
  else    
-   echo -e " mysql already $Y INSTALLED" &>>$OG_FILE_NAME
+   echo -e " mysql already $Y INSTALLED" 
    fi
 
    #Installing the git
-   dnf list installed git &>>$OG_FILE_NAME
+   dnf list installed git  &>> "$LOG_FILE_NAME"
    if [ $? -ne 0 ] ;then
-   dnf install git 
+   dnf install git -y &>> "$LOG_FILE_NAME" 
    VALIDATE $? "Installing Git"
-    else 
-     echo -e " Git already $Y installed "   
+   else 
+     echo -e "$N Git already $Y INSTALLED"   
      fi
 
