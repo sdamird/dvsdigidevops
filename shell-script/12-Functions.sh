65d0baece29d1f394869fef85c61089b80fd98fb
sdamird <damiaws2024@gmail.com>
2025-01-22 11:19:41 +0530

    add

---
+++ shell-script/12-Functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+USERID=$(id -u)
+VALIDATE(){
+     if [ $1 -ne 0 ];then 
+    echo " $2 ...FAILURE"
+    exit 1
+    else 
+    echo " $2  Success"
+    fi
+    else 
+}
+if [ "$USERID" -ne 0 ];then 
+  echo "Error: you must have sudo privilge to install the package"
+  exit 1 #other than 0
+  fi
+  dnf list installed mysql
+ if [ $? -ne 0 ] ;then
+   dnf install mysql -y 
+   VALIDATE $? "Installing MySql"
+       
+   echo " mysql already installed "
+   fi
+
+   #Installing the git
+   dnf list installed git 
+   VALIDATE $? "Installing Git"
+    else 
+     echo " Git already installed "
+     fi
+
